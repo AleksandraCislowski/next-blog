b4dac3d1d18b172939650ba4c275eb80b4416cf3
aleksandra <aleksandra.swigon91@gmail.com>
2024-02-28 20:10:31 +0100

    post detail

---
+++ components/posts/post-content.js
@@ -0,0 +1,23 @@
+import classes from "../../styles/post-content.module.css";
+import PostHeader from "./post-header";
+
+const DUMMY_POST = {
+  title: "Getting started with NextJS.",
+  image: "getting-started-nextjs.png",
+  date: "2022-02-10",
+  slug: "getting-started",
+  content: "#This is a first post",
+};
+
+function PostContent() {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      {DUMMY_POST.content}
+    </article>
+  );
+}
+
+export default PostContent;
