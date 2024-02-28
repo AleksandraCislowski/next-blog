53d8ccacfc2df43ab2f9f141a86fc3b33cec35e6
aleksandra <aleksandra.swigon91@gmail.com>
2024-02-28 18:45:30 +0100

    hero

---
+++ components/home-page/featured-posts.js
@@ -0,0 +1,6 @@
+import classes from "../../styles/featured-posts.module.css";
+
+function FeaturedPosts() {
+  return <section></section>;
+}
+export default FeaturedPosts;
---
+++ components/home-page/hero.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import classes from "../../styles/hero.module.css";
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <Image
+        className={classes.image}
+        src='/images/site/me.jpg'
+        alt='An image showing Alex'
+        width={300}
+        height={300}
+      />
+      <h1>Hello, I'm Alex!</h1>
+      <p>I might be even blogging something here!</p>
+    </section>
+  );
+}
+export default Hero;
