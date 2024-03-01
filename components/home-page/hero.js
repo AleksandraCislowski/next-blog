5325d17a32a3ef153465b2cb9dbe38aa5c89e212
aleksandra <aleksandra.swigon91@gmail.com>
2024-03-01 22:25:04 +0100

    hotfix

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -11,7 +11,7 @@ function Hero() {
         width={300}
         height={300}
       />
-      <h1>Hello, I'm Aleksandra!</h1>
+      <h1>Hello, my name is Aleksandra!</h1>
       <p>I might be even blogging something here!</p>
     </section>
   );
