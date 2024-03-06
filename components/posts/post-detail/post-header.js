b77a8870f546d6611b7c2d3b898452033b2b51b7
aleksandra <aleksandra.swigon91@gmail.com>
2024-03-06 11:46:55 +0100

    post detail

--- components/posts/post-detail/post-header.js
+++ components/posts/post-detail/post-header.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import classes from "../../styles/post-header.module.css";
+import classes from "../../../styles/post-header.module.css";
 
 function PostHeader(props) {
   const { title, image } = props;
