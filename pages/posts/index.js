25a064820913937364be940988636440641970bf
aleksandra <aleksandra.swigon91@gmail.com>
2024-03-06 19:43:16 +0100

    metadata

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,21 @@
+import { Fragment } from "react";
 import AllPosts from "../../components/posts/all-posts";
 import { getAllPosts } from "../../lib/posts-util";
+import Head from "next/head";
 
 function AllPostsPage(props) {
-  return <AllPosts posts={props.posts} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>All My Posts</title>
+        <meta
+          name='description'
+          content='A list of all travelling-related posts'
+        />
+      </Head>
+      <AllPosts posts={props.posts} />
+    </Fragment>
+  );
 }
 
 export function getStaticProps() {
