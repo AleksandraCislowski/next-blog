e69d1d9f020994f0d45b73119171ef0e3bc37e05
aleksandra <aleksandra.swigon91@gmail.com>
2024-03-06 10:53:02 +0100

    markdown for rendering posts

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,25 @@
 import { Fragment } from "react";
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-const DUMMY_POST = [
-  {
-    title: "Getting started with NextJS.",
-    image: "getting-started-nextjs.png",
-    date: "2022-02-10",
-    slug: "getting-started",
-    excerpt: "NextJS is the React framework for production.",
-  },
-  {
-    title: "Getting started with NextJS.",
-    image: "getting-started-nextjs.png",
-    date: "2022-02-10",
-    slug: "getting-started22",
-    excerpt: "NextJS is the React framework for production.",
-  },
-  {
-    title: "Getting started with NextJS.",
-    image: "getting-started-nextjs.png",
-    date: "2022-02-10",
-    slug: "getting-started3",
-    excerpt: "NextJS is the React framework for production.",
-  },
-  {
-    title: "Getting started with NextJS.",
-    image: "getting-started-nextjs.png",
-    date: "2022-02-10",
-    slug: "getting-starte4",
-    excerpt: "NextJS is the React framework for production.",
-  },
-];
-
-function HomePage() {
+function HomePage(props) {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POST} />
+      <FeaturedPosts posts={props.posts} />
     </Fragment>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
 }
 
 export default HomePage;
