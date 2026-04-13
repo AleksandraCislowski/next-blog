8f22a72cfd9fcb53895c060078bb8d0ad92328cc
aleksandra <cislowski.aleksandra@gmail.com>
2026-04-13 19:22:28 +0200

    feat: update Hero component with new coastal scene image and enhanced styles

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,18 +1,24 @@
 import Image from "next/image";
 import classes from "../../styles/hero.module.css";
+import coastalScene from "../../public/images/site/coastal-scene.png";
 
 function Hero() {
   return (
     <section className={classes.hero}>
       <Image
         className={classes.image}
-        src='/images/site/me.jpg'
-        alt='An image showing Aleksandra - owner of this blog.'
-        width={300}
-        height={300}
+        src={coastalScene}
+        alt='An illustrated coastal landscape for Elsewhere Log.'
+        fill
+        priority
+        sizes='100vw'
       />
-      <h1>Elsewhere Log</h1>
-      <p>Travel notes from places worth remembering.</p>
+      <div className={classes.overlay} />
+      <div className={classes.content}>
+        <h1>Elsewhere Log</h1>
+        <p>Travel notes from places worth remembering.</p>
+        <a href='#featured-notes'>Read the notes</a>
+      </div>
     </section>
   );
 }
