8c5171e13052c98cafc0f8636382674bd6412e1e
aleksandra <cislowski.aleksandra@gmail.com>
2026-04-18 14:16:26 +0200

    Add portfolio link to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -4,6 +4,7 @@ import Seo from "../components/seo/seo";
 
 const contactEmail = "cislowski.aleksandra@gmail.com";
 const emailSubject = "Hello from Elsewhere Log";
+const portfolioUrl = "https://aleksandracislowski.com";
 
 function ContactPage() {
   const mailLink = `mailto:${contactEmail}?subject=${encodeURIComponent(
@@ -24,14 +25,15 @@ function ContactPage() {
           <h1>Let&apos;s talk about travel, frontend work, or useful little tools.</h1>
           <p>
             I like thoughtful projects, places with a story, and tools that make
-            everyday things easier. Send a note if you want to connect.
+            everyday things easier. If you want the bigger picture, my portfolio
+            has more of the work I am proud of.
           </p>
           <div className={classes.actions}>
             <a className={classes.primaryAction} href={mailLink}>
               Send an email
             </a>
-            <a className={classes.secondaryAction} href='/posts'>
-              Browse travel notes
+            <a className={classes.secondaryAction} href={portfolioUrl}>
+              Visit my portfolio
             </a>
           </div>
           <p className={classes.emailNote}>
